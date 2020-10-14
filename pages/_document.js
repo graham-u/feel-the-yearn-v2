6bbee0573b01076b3d3382c898bea52a72f6604f
Graham <grahamu@gmail.com>
2020-10-14 18:45:38 +0100

    Add title.

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Feel the Yearn</title>
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
