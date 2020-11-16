bed716754a6c3336d9f7f9958693994cf06c51e3
Graham <grahamu@gmail.com>
2020-11-16 20:43:42 +0000

    Ensure long strategy links cut off gracefully.

--- components/vaultsReport/vaultOverview/strategyLink/index.js
+++ components/vaultsReport/vaultOverview/strategyLink/index.js
@@ -3,7 +3,7 @@ import Link from "@material-ui/core/Link";
 function StrategyLink({ address, linkText, titleText }) {
   const url = `https://etherscan.io/address/${address}#code`;
   return (
-    <Link href={url} title={titleText} variant="body1">
+    <Link href={url} title={titleText} variant="body1" display="block" noWrap>
       {linkText}
     </Link>
   );
