91867a5d006967df4ecc288c7d078a0c56cbc43e
Graham <grahamu@gmail.com>
2020-11-18 09:39:16 +0000

    Fix vault link.

--- components/vaultsReport/vaultOverview/vaultLink/index.js
+++ components/vaultsReport/vaultOverview/vaultLink/index.js
@@ -3,7 +3,7 @@ import Link from "@material-ui/core/Link";
 function VaultLink({ address, linkText, titleText }) {
   const url = `https://etherscan.io/address/${address}#code`;
   return (
-    <Link href={url} title={titleText} variant="h5" component="h2">
+    <Link href={url} title={titleText} variant="h5">
       {linkText}
     </Link>
   );
