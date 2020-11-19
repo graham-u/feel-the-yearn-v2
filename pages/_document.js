1845834b2dc03427e5b915936843d7acdf10668b
Graham <grahamu@gmail.com>
2020-11-19 22:41:09 +0000

    Add analytics.

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,9 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+// Analytics tracking.
+const clickyId = process.env.NEXT_PUBLIC_CLICKY_ID;
+const clickyAnalyticsCode = `var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(${clickyId});`;
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -15,6 +19,8 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <script dangerouslySetInnerHTML={{ __html: clickyAnalyticsCode }} />
+          <script async src="//static.getclicky.com/js"></script>
         </Head>
         <body>
           <Main />
