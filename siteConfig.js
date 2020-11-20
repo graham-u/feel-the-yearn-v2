11ea63be0c18f88d599a71df887808209c7f8b97
Graham <grahamu@gmail.com>
2020-11-20 23:01:36 +0000

    Improve accuracy of some error messages.

--- siteConfig.js
+++ siteConfig.js
@@ -1,7 +1,7 @@
 const notificationMessages = {
-  userStatsFetchFailed: "Failed to fetch user stats. Lifetime earnings cannot be shown.",
+  userStatsFetchFailed: "Failed to fetch user stats. Lifetime earnings may be missing / stale.",
   vaultRegistryFetchFailed: "Failed to fetch vault registry data. Vaults cannot be shown.",
-  tokenPricesFetchFailed: "Failed to fetch token prices. Fiat balances cannot be shown.",
+  tokenPricesFetchFailed: "Failed to fetch token prices. Fiat balances may be missing / stale.",
   vaultApyStatsFetchFailed:
     "Failed to fetch vault performance stats. Vault returns cannot currently be shown.",
 };
