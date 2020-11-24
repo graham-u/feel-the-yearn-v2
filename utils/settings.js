d19fb58d32fd197776b1f38584f9eefc66c34ec4
Graham <grahamu@gmail.com>
2020-11-24 20:02:02 +0000

    Improve settings persistence code to store dat as JSON and log errors.

--- utils/settings.js
+++ utils/settings.js
@@ -1,16 +1,29 @@
+const getSettingsKey = (name) => `setting:${name}`;
+
 function loadSetting(name) {
-  if (typeof window === "undefined") {
+  if (typeof window === "undefined") return null;
+
+  try {
+    const serializedValue = localStorage.getItem(getSettingsKey(name));
+    if (serializedValue === null) {
+      return null;
+    }
+    return JSON.parse(serializedValue);
+  } catch (err) {
+    console.log(err);
     return null;
   }
-  return localStorage.getItem(name);
 }
 
 function saveSetting(name, value) {
-  if (typeof window === "undefined") {
-    return null;
-  }
+  if (typeof window === "undefined") return null;
 
-  localStorage.setItem(name, value);
+  try {
+    const serializedValue = JSON.stringify(value);
+    localStorage.setItem(getSettingsKey(name), serializedValue);
+  } catch {
+    console.log(err);
+  }
 }
 
 export { loadSetting, saveSetting };
