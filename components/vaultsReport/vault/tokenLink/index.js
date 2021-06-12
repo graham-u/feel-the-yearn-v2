c24ff199d197f68e4c0b11f86c29de2ffe244e09
Graham <grahamu@gmail.com>
2021-06-12 23:11:05 +0100

    Show token symbol instead of icon when theme dictates not to show icons

--- components/vaultsReport/vault/tokenLink/index.js
+++ components/vaultsReport/vault/tokenLink/index.js
@@ -1,14 +1,29 @@
+import { useTheme } from "@material-ui/core";
 import { ThemedLink as Link } from "components/Link";
 
 function TokenLink({ token }) {
+  const theme = useTheme();
+
   if (!token) {
     return false;
   }
 
+  const linkContent = theme.vaults.showIcons ? (
+    <img
+      src={token.icon}
+      alt={token.symbol}
+      height={16}
+      width={16}
+      style={{ verticalAlign: "text-top" }}
+    />
+  ) : (
+    token.symbol
+  );
+
   const url = `https://etherscan.io/address/${token.address}`;
   return (
     <Link href={url} title={token.name} variant="body1">
-      <img src={token.icon} height={16} width={16} style={{ verticalAlign: "text-top" }} />
+      {linkContent}
     </Link>
   );
 }
